5ed16d632085ada57ef16d461948de63bd6e33aa
Adams Paul <adamsohiani@gmail.com>
2020-04-12 07:28:42 +0100

    Updated again to perform checks

--- src/estimator.php
+++ src/estimator.php
@@ -4,6 +4,20 @@ require_once __DIR__.'/impact.php';
 
 require_once __DIR__.'/severeImpact.php';
 
+$data = array(
+  'region' => [
+    'name' => 'Africa',
+    'avgAge' => 19.7,
+    'avgDailyIncomeInUSD' => 5,
+    'avgDailyIncomePopulation' => 0.71
+  ],
+  'periodType' => "days",
+  'timeToElapse' => 30,
+  'reportedCases' =>50,
+  'population' => 50000,
+  'totalHospitalBeds' =>45545
+);
+
 function covid19ImpactEstimator($data)
 {
 
@@ -11,49 +25,37 @@ function covid19ImpactEstimator($data)
 
   $severeImpact = new SevereImpact();
 
-  $data = array(
-    'region' => [
-      'name' => 'Africa',
-      'avgAge' => 19.7,
-      'avgDailyIncomeInUSD' => 5,
-      'avgDailyIncomePopulation' => 0.71
-    ],
-    'periodType' => "days",
-    'timeToElapse' => 30,
-    'reportedCases' =>50,
-    'population' => 50000,
-    'totalHospitalBeds' =>45545
-);
-  if ($data->periodType == "days") {
-    $numberOfDays = $data->timeToElapse;
+  
+  if ($data['periodType'] == "days") {
+    $numberOfDays = $data['timeToElapse'];
   } 
-  else if ($data->periodType == "weeks") {
-    $numberOfDays = 7 * $data->timeToElapse;
+  else if ($data['periodType'] == "weeks") {
+    $numberOfDays = 7 * $data['timeToElapse'];
   }
-  else if ($data->periodType == "months") {
-    $numberOfDays = 30 * $data->timeToElapse;
+  else if ($data['periodType'] == "months") {
+    $numberOfDays = 30 * $data['timeToElapse'];
   }
   else{
     return 'period types include days,weeks and months';
   }
 
-  $impact->currentlyInfected = $data->reportedCases * 10;
+  $impact->currentlyInfected = $data['reportedCases'] * 10;
 
-  $severeImpact->currentlyInfected = $data->reportedCases * 50;
+  $severeImpact->currentlyInfected = $data['reportedCases'] * 50;
 
   $ipower = $numberOfDays / 3;
 
   $spower = $numberOfDays / 3;
 
-  $impact->infectionsByRequestedTime = $data->currentlyInfected * (2**$ipower);
+  $impact->infectionsByRequestedTime = $data['currentlyInfected'] * (2**$ipower);
 
-  $severeImpact->infectionsByRequestedTime = $data->currentlyInfected * (2**$spower);
+  $severeImpact->infectionsByRequestedTime = $data['currentlyInfected'] * (2**$spower);
 
   $impact->severeCasesByRequestedTime = 0.15 * $impact->infectionsByRequestedTime;
 
   $severeImpact->severeCasesByRequestedTime = 0.15 * $severeImpact->infectionsByRequestedTime;
 
-  $bedAvailable = 0.35 * $data->totalHospitalBeds;
+  $bedAvailable = 0.35 * $data['totalHospitalBeds'];
 
   $impact->hospitalBedsByRequestedTime = intval($bedAvailable - $impact->severeCasesByRequestedTime);
 
